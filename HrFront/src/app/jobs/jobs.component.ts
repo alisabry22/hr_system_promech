2663c3721a028ff16af7854e616e5ec9c10a3533
alisabry22 <alysabry722@gmail.com>
2023-07-30 17:00:32 +0300

    dd

---
+++ HrFront/src/app/jobs/jobs.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { JobService } from '../services/job-service';
+
+@Component({
+  selector: 'app-jobs',
+  templateUrl: './jobs.component.html',
+  styleUrls: ['./jobs.component.css']
+})
+export class JobsComponent implements OnInit{
+  constructor(private jobservice:JobService){
+    
+  }
+  ngOnInit(): void {
+
+  }
+
+}
